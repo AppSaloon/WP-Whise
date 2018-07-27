2802157628761785583435ab69bf7e151a2c9f8d
Aytac Kokus <aytac@appsaloon.be>
2018-07-27 11:14:12 +0200

    Added comment to Project model

--- model/project.php
+++ model/project.php
@@ -4,14 +4,41 @@ namespace wp_whise\model;
 
 class Project {
 
+	/**
+	 * Project ID
+	 *
+	 * @var int
+	 *
+	 * @since 1.0.0
+	 */
 	public $post_id;
 
-	public $attachment_ids;
-
+	/**
+	 * All project meta data
+	 *
+	 * @var array
+	 *
+	 * @since 1.0.0
+	 */
 	public $post_meta = array();
 
+	/**
+	 * Only update meta data
+	 * This is handy to save amount of query
+	 *
+	 * @var array
+	 *
+	 * @since 1.0.0
+	 */
 	public $updated_post_meta = array();
 
+	/**
+	 * Fields to ignore from the backend fields
+	 *
+	 * @var array
+	 *
+	 * @since 1.0.0
+	 */
 	private $ignore_fields = array(
 		'_edit_lock',
 		'_edit_last',
@@ -22,16 +49,37 @@ class Project {
 		'_ajax_nonce-add-estate-category'
 	);
 
+	/**
+	 * Project constructor.
+	 *
+	 * Currently does nothing
+	 */
 	public function __construct() {
 
 	}
 
+	/**
+	 * Get the project data
+	 *
+	 * @param $post_id
+	 *
+	 * @since 1.0.0
+	 */
 	public function set_post( $post_id ) {
 		$this->post_id = $post_id;
 
 		$this->post_meta = get_post_meta( $post_id );
 	}
 
+	/**
+	 * This method is called after saving the project in the backend.
+	 *
+	 * Here we do filter the meta keys so only custom fields are saved
+	 *
+	 * @param $post_data    array
+	 *
+	 * @since 1.0.0
+	 */
 	public function set_post_data( $post_data ) {
 		foreach ( $post_data as $key => $value ) {
 			/**
@@ -41,6 +89,9 @@ class Project {
 			     && substr( $key, 0, 4 ) !== '_acf'
 			     && ! in_array( $key, $this->ignore_fields )
 			) {
+				/**
+				 * custom field does not exist or the value is not the same
+				 */
 				if ( ! isset( $this->post_meta[ $key ] ) || $value != $this->post_meta[ $key ] ) {
 					$this->post_meta[ $key ]         = $value;
 					$this->updated_post_meta[ $key ] = $value;
@@ -49,22 +100,49 @@ class Project {
 		}
 	}
 
+	/**
+	 * Sets gallery image ids
+	 *
+	 * @param $attachment_ids
+	 *
+	 * @since 1.0.0
+	 */
 	public function set_gallery_image_ids( $attachment_ids ) {
-		$this->attachment_ids = $attachment_ids;
+		$this->post_meta['_gallery_image_ids'] = $attachment_ids;
 
-		$this->post_meta['_gallery_image_ids'] = $this->attachment_ids;
-
-		$this->updated_post_meta['_gallery_image_ids'] = $this->attachment_ids;
+		$this->updated_post_meta['_gallery_image_ids'] = $attachment_ids;
 	}
 
+	/**
+	 * Returns gallery image ids
+	 *
+	 * @return mixed
+	 *
+	 * @since 1.0.0
+	 */
 	public function get_gallery_image_ids() {
 		return maybe_unserialize( $this->post_meta['_gallery_image_ids'][0] );
 	}
 
+	/**
+	 * Returns meta key
+	 * If it does not exist then it will return empty string
+	 *
+	 * @param $meta_key string  Meta key to look for
+	 *
+	 * @return string   Meta key or empty string
+	 *
+	 * @since 1.0.0
+	 */
 	public function get_meta( $meta_key ) {
 		return ( isset( $this->post_meta[ $meta_key ] ) ) ? $this->post_meta[ $meta_key ][0] : '';
 	}
 
+	/**
+	 * Save post meta
+	 *
+	 * @since 1.0.0
+	 */
 	public function save() {
 		if ( is_array( $this->updated_post_meta ) && count( $this->updated_post_meta ) != 0 ) {
 			foreach ( $this->updated_post_meta as $meta_key => $meta_value ) {
